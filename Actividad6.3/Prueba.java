e17b6824618aa58540827f4b639f7679b0a39a04
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:44:15 -0500

    Create Prueba.java

---
+++ Actividad6.3/Prueba.java
@@ -0,0 +1,9 @@
+package profesores;
+
+public class Prueba {
+    public static void main(String[] args) {
+    Profesor profesor1 = new ProfesorTitular();
+    profesor1.imprimir();
+    
+    }
+}
