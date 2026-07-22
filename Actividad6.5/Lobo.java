8801346a0460f7ecec92e8a9474c2fd7a69e367c
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:53:45 -0500

    Create Lobo.java

---
+++ Actividad6.5/Lobo.java
@@ -0,0 +1,16 @@
+package actividad6.pkg6;
+
+public class Lobo extends Canido {
+    public String getSonido() {
+        return "Aullido";
+    }
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+    public String getHábitat() {
+        return "Bosque";
+    }
+    public String getNombreCientífico() {
+        return "Canis lupus";
+    }
+}
