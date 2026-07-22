b5b244111eaccd529e6b80a9dedcbd979d225642
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:51:09 -0500

    Create Gato.java

---
+++ Actividad6.5/Gato.java
@@ -0,0 +1,19 @@
+package actividad6.pkg6;
+
+public class Gato extends Felino {
+    public String getSonido() {
+        return "Maullido";
+    }
+
+    public String getAlimentos() {
+        return "Ratones";
+    }
+    
+    public String getHábitat() {
+        return "Doméstico";
+    }
+
+    public String getNombreCientífico() {
+        return "Felis silvestris catus";
+    }
+}
