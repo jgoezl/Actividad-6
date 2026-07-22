742159a798cbed2ec5652d9b9df7719198035afc
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:45:21 -0500

    Create Profesor.java

---
+++ Actividad6.4/Profesor.java
@@ -0,0 +1,17 @@
+package actividad6.pkg4;
+
+public class Profesor {
+    protected void imprimir() {
+        System.out.println("Es un profesor.");
+    }
+    
+    void espaciado() {
+        System.out.println("========\n");
+        System.out.println("========\n");
+    }
+
+    void imprimirAños() {
+        int años = 0;
+        System.out.println("Años = " + años);
+    }
+}
