ba26523c7bd23fcdfcbc9c06eed453a3cfec81ce
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:49:57 -0500

    Create Animal.java

---
+++ Actividad6.5/Animal.java
@@ -0,0 +1,13 @@
+package actividad6.pkg6;
+
+public abstract class Animal {
+    protected String sonido; 
+    protected String alimentos; 
+    protected String hábitat;
+    protected String nombreCientífico;
+    
+    public abstract String getNombreCientífico();
+    public abstract String getSonido();
+    public abstract String getAlimentos();
+    public abstract String getHábitat();
+}
