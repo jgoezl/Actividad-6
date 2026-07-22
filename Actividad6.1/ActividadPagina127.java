3d4f47090437f4ab13cecb8ba186547bf2b93d68
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:39:02 -0500

    Create ActividadPagina127.java

---
+++ Actividad6.1/ActividadPagina127.java
@@ -0,0 +1,16 @@
+package actividad6.pkg1;
+ 
+public class ActividadPagina127 {
+ 
+    public static void main(String[] args) {
+
+        Pedido pedido1 = new Pedido();
+        pedido1.calcularPedido("Sancocho", 5000, "Gaseosa", 2000);
+
+        Pedido pedido2 = new Pedido();
+        pedido2.calcularPedido("Crema de verduras", 5000, "Churrasco", 6000, "Gaseosa", 2000);
+
+        Pedido pedido3 = new Pedido();
+        pedido3.calcularPedido("Crema de espinacas", 5000, "Salmón", 10000, "Tiramisú", 5000, "Gaseosa", 2000);
+    }
+}
