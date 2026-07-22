c59e5b71c3fcbd2f0b9782e6cb66e86d9bd5b562
jgoezl <jgoezl@unal.edu.co>
2026-07-21 20:45:02 -0500

    Create Prueba3.java

---
+++ Actividad6.4/Prueba3.java
@@ -0,0 +1,15 @@
+package actividad6.pkg4;
+
+public class Prueba3 {
+    public static void main(String[] args) {
+        Profesor profesor1 = new ProfesorTitular();
+        profesor1.imprimirAños();
+        profesor1.imprimir();
+        profesor1.espaciado();
+        
+        ProfesorTitular profesor2 = new ProfesorTitular();
+        profesor2.imprimirAños();
+        profesor2.imprimir();
+        profesor2.espaciado();        
+    }
+}
